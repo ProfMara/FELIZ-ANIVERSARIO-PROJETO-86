fdb090949b497521bf0f4ac6ad8b10d037018f17
ProfMara <94469709+ProfMara@users.noreply.github.com>
2022-12-09 15:57:34 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+
+
+var canvas = new fabric.Canvas('canvas');
+var x= document.getElementById("musica");
+
+
+function novaImagem(){
+
+
+	
+}
+
+function playSound(){
+    //é aqui que coloca o som para tocar a musica
+
+}
